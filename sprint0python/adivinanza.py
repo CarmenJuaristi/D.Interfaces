a40e4bb3a200aa7a91397f4ecacb2286a4030675
CarmenJuaristi <145338586+CarmenJuaristi@users.noreply.github.com>
2023-09-28 18:25:29 +0200

    Add files via upload

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,1 +1,8 @@
-
+print ("")
+print ("Te sirven para escribir, dibujar, señalar y sentir, qué son?")
+solucion = input ("a. Un lapiz b.Los dedos c.Los pies")
+print ("")
+if (solucion != 'b'):
+    print ("Perdiste. Esa respuesta no es la correcta.\n")
+else:
+    print("¡Acertaste!.\n")
