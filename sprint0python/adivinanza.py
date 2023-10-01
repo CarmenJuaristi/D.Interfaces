249a4e841cec1008ba27f0f5468d4bc4f68bbf98
CarmenJuaristi <145338586+CarmenJuaristi@users.noreply.github.com>
2023-10-01 20:59:15 +0200

    Add files via upload

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,30 +1,44 @@
-print ("")
-print ("Te sirven para escribir, dibujar, señalar y sentir, qué son?")
-solucion = input ("a. Un lapiz b.Los dedos c.Los pies")
-print ("")
-puntuacion=0
-if (solucion != 'b'):
-    print ("Perdiste. Esa respuesta no es la correcta.\n")
-    puntuacion = puntuacion - 5
-else:
-    print("¡Acertaste!.\n")
-    puntuacion = puntuacion + 10
-print ("Cuando llueve y sale el sol, todos los colores tengo yo. Qué soy?")
-solucion = input ("a.Una nuve b.La luna c.Un arcoiris")
-if (solucion != 'c'):
-    print ("Perdiste. Esa respuesta no es la correcta.\n")
-    puntuacion = puntuacion - 5
-else:
-    print("¡Acertaste!.\n")
-    puntuacion = puntuacion + 10
-print ("Blanca por dentro, verde por fuera. Si no sabes, espera. Qué soy?")
-solucion = input ("a.Una pera b.Una manzana c.Una oreo")
-if (solucion != 'a'):
-    print ("Perdiste. Esa respuesta no es la correcta.\n")
-    puntuacion = puntuacion - 5
-else:
-    print("¡Acertaste!.\n")
-    puntuacion = puntuacion + 10
+import random
 
-print ("Fin del juego. La puntuación total es: "+str(puntuacion))
+puntos = 0
+veces = 0
 
+adivinanzas = {
+    1: "Te sirven para escribir, dibujar, señalar y sentir, qué son? a. Un lapiz b.Los dedos c.Los pies",
+    2: "Cuando llueve y sale el sol, todos los colores tengo yo. Qué soy? a.Una nuve b.La luna c.Un arcoiris",
+    3: "Blanca por dentro, verde por fuera. Si no sabes, espera. Qué soy? a.Una pera b.Una manzana c.Una oreo",
+}
+
+claves_adivinanzas = list(adivinanzas.keys())
+
+while veces < 3:
+    sel = random.sample(claves_adivinanzas, 1)[0]
+    claves_adivinanzas.remove(sel) 
+
+    print(adivinanzas[sel])
+    sol = input("\t")
+    veces += 1
+
+    if sel == 1:
+        if sol == 'b':
+            print("\n¡Enhorabuena! Has acertado.\n")
+            puntos += 10
+        else:
+            print("\nError. Esa es la respuesta incorrecta.\n")
+            puntos -= 5
+    elif sel == 2:
+        if sol == 'c':
+            print("\n¡Enhorabuena! Has acertado.\n")
+            puntos += 10
+        else:
+            print("\nError. Esa es la respuesta incorrecta.\n")
+            puntos -= 5
+    elif sel == 3:
+        if sol == 'a':
+            print("\n¡Enhorabuena! Has acertado.\n")
+            puntos += 10
+        else:
+            print("\nError. Esa es la respuesta incorrecta.\n")
+            puntos -= 5
+
+print("Ha finalizado el juego. Felicidades, tienes un total de: " + str(puntos) + "\n")
