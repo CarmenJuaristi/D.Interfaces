c97fd7a7321345d867a974ca14bf11a394c6ce61
CarmenJuaristi <cjuaristip22@fpcoruna.afundacion.org>
2023-10-27 12:13:55 +0200

    Corregido el ejercicio 2 del segundo sprint

--- sprint2catalog/catalog/data/loadingWindow.py
+++ sprint2catalog/catalog/data/loadingWindow.py
@@ -7,16 +7,22 @@ from tkinter import messagebox
 class loadingWindow:
     
     def __init__(self, root):
+        self.finished = False
+        self.json_data = []
         self.root = root
-        self.root.title("CARGANDO...") # Aqui le damos el titulo a la ventana de carga
-        self.root.geometry("170x120") # Aqui le damos el tamaño a la ventana de carga
-        self.root.resizable(False, False) # Aqui indicamos que no se puede estirar ni ampliar la ventana de carga
-        
-        self.label = tk.Label(self.root, text="Cargando datos...", font=("arial", 14)) # Aqui mostramos un texto dentro de la ventana
+         # Esto es el título de la ventana de carga
+        self.root.title("CARGANDO...")
+        # Aquí indicamos el ancho y alto de dicha ventana 
+        self.root.geometry("170x120") 
+        # Aquí lo que indicamos es que no se pueda aumentar el tamaño de la ventana ni en horizontal ni en vertical
+        self.root.resizable(False, False) 
+        # Esto es el texto que hay dentro de la ventana
+        self.label = tk.Label(self.root, text="Cargando datos...", font=("arial", 14)) 
         self.label.pack(side=tk.TOP, pady=10)
-# De aqui para aabajo hacemos el circulo de carga
-        label_bg_color = self.label.cget("bg")
-
+        # A partir de aquí creamos el círculo de carga
+        label_bg_color = self.label.cget("bg") #Esto es para obtener el color de fondo 
+        
+        #Aquí dibujamos la circunferencia
         self.canvas = tk.Canvas(self.root, width=60, height=60, bg=label_bg_color)
         self.canvas.pack()
 
@@ -28,14 +34,16 @@ class loadingWindow:
 
         self.thread = threading.Thread(target=self.fetch_json_data)
         self.thread.start()
-        
-    def draw_progress_circle(self, progress): # Aqui mostramos el circulo de carga
+        if self.thread.is_alive():
+            self.check_thread()
+    # Aquí lo mostramos 
+    def draw_progress_circle(self, progress): 
         self.canvas.delete("progress")
         angle = int(360 * (progress/100))
-
         self.canvas.create_arc(10, 10, 35, 35, start = 0, extent=angle, tags="progress", outline='green', width=5, style=tk.ARC)
     
-    def update_progress_circle(self): # Aqui hacemos que el circulo de carga gire
+    # Aquí hacemos que gire el círculo de carga 
+    def update_progress_circle(self): 
         if self.progress < 100:
             self.progress+=10
         else:
@@ -44,16 +52,25 @@ class loadingWindow:
         self.draw_progress_circle(self.progress)
         self.root.after(200, self.update_progress_circle)
     
-    def launch_main_window(json_data, root): # Aqui lanzamos la ventana main
-        root = tk.Tk()
-        app = MainWindow(root, json_data) #Aqui lanzamos la ventana principal
-        root.mainloop()
-
-    def fetch_json_data(self, json_data): # Aqui descargamos el json
-        response = requests.get("https://github.com/CarmenJuaristi/D.Interfaces/blob/main/catalog.json")
+    #Aquí será donde en el siguiente ejercicio descarguemos el json
+    def fetch_json_data(self): 
+        response = requests.get("json_alojado_en_GitHub")
         if response.status_code == 200:
-            json_data = response.json()
-            self.root.quit()
-            self.launch_main_window(json_data)   
+            self.json_data = response.json()
+            self.finished=True
+    
+    # Aquí comprobamos que la descarga esté bien
+    def check_thread(self): 
+        if self.finished:
+            self.root.destroy()
+            launch_main_window(self.json_data)
+        else:
+            self.root.after(100, self.check_thread)
 
+# Aquí lanzamos el main
+def launch_main_window(json_data):
+    root = tk.Tk()
+    # Y aquí lanzamos la principal
+    app = MainWindow(root, json_data) 
+    root.mainloop()
     
