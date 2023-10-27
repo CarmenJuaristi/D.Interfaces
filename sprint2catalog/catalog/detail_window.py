71d6e630ce16e1eb59ae32955cb2f232f9b38a9d
CarmenJuaristi <cjuaristip22@fpcoruna.afundacion.org>
2023-10-28 00:20:34 +0200

    Completada tarea 7 no me dió tiempo a comentar bien los ejercicios

--- sprint2catalog/catalog/detail_window.py
+++ sprint2catalog/catalog/detail_window.py
@@ -1,33 +1,18 @@
 import tkinter as tk 
 from tkinter import ttk
 from cell import Cell
-class DetailWindow:
-    def __init__ (self,root,title,image,description):
-        self.root = root
-        self.title = title
-        self.image = image
-        self.description = description
-        self.window = tk.Toplevel(root)
-        self.window.title(self.title)
-    # Para la nueva pestaña que abriremos poniendo la foto, título y descripción primero ponemos al imagen, después el tipo de letra que querremos en el titulo y la descripcion
-        image_label = ttk.Label(self.window, image = self.image)
-        image_label.pack()
-        title_label = ttk.Label(self.window, text = self.title, font = ("Times New Roman",16))
-        title_label.pack()
-        description_label = ttk.Label(self.window, text = self.description, wraplength= 300)
-        description_label.pack()
 
-    def mostrarDetalles(cell):
-        root = tk.Toplevel()
-        root.title(cell.nombre)
-        mostrarl = ttk.Label(root, image = cell.image_tk)
-        mostrarl.pack()
-        mostrarl2 = ttk.Label(root, text = cell.descripcion)
-        mostrarl2.pack()
-        #Aquí vamos a centrar la imagen
-        x = (root.winfo_screenwidth() - root.winfo_reqwidth()) /2 
-        y = (root.winfo_screenheight() - root.winfo_reqheight()) /2
-        root.geometry(f"+{int(x)}+{int(y)}")
-        # Aquí lannzaremos la ventana detallada
-        root.mainloop() 
+def mostrarDetalles(cell):
+    root = tk.Toplevel()
+    root.title(cell.nombre)
+    mostrarl = ttk.Label(root, image = cell.image_tk)
+    mostrarl.pack()
+    mostrarl2 = ttk.Label(root, text = cell.descripcion)
+    mostrarl2.pack()
+    #Aquí vamos a centrar la imagen
+    x = (root.winfo_screenwidth() - root.winfo_reqwidth()) /2 
+    y = (root.winfo_screenheight() - root.winfo_reqheight()) /2
+    root.geometry(f"+{int(x)}+{int(y)}")
+    # Aquí lannzaremos la ventana detallada
+    root.mainloop() 
     
--- sprint2catalog/catalog/window.py
+++ sprint2catalog/catalog/window.py
@@ -5,13 +5,13 @@ from PIL import Image, ImageTk
 import tkinter as tk
 from cell import Cell
 from tkinter import messagebox, Canvas, Frame, Label, Tk, ttk
-from detail_window import DetailWindow
+from detail_window import mostrarDetalles
 class MainWindow:
     cells = []
     # Primero haremos que cuando clickee una de las fotos le salte el mensaje de hiciste click en la celda de y el título de la celda que en este caso será el titulo del 
     # videojuego en el que hagas click
     def on_button_clicked(self,cell):
-        DetailWindow(self.root, cell.title, cell.image_url, cell.description)
+        mostrarDetalles(self.root, cell.title, cell.image_url, cell.description)
     def __init__(self, root, json_data):
         self.root = root
         self.cells = []
@@ -21,6 +21,32 @@ class MainWindow:
             image_url = i.get("image_url")
             cell = Cell(title,description,image_url)
             self.cells.append(cell)
+        #Crearemos aquí el scrollbar, que será verical y tambien el tamaño que queremos 
+        self.canvas = tk.Canvas(root, highlightthickness=0)
+        self.scrollbar = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview, width=15)
+        # Crearemos el Frame del scrollbar
+        self.scrollable_frame = tk.Frame(self.canvas)
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+        # Aqui lo configuraremos
+        self.canvas.create_window((0,0), window=self.scrollable_frame, anchor="nw")
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        
+        for i, cell in enumerate(self.cells):
+            frame = tk.Frame(self.scrollable_frame)
+            frame.pack(pady=10)
+            label = tk.Label(frame, image=cell.image_tk, text=cell.nombre, compound=tk.BOTTOM)
+            label.grid(row=i, column=0)
+            label.bind("<Button-1>", lambda event, celda= cell:mostrarDetalles(celda))
+        # Y por último lo posicionamos en la ventana
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.scrollbar.grid(row=0, column=1, sticky="ns")
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1) 
         # Aquí lo que hacemos es enumerar las celdas, es decir que estén ordenadas en vertical
         for i in range in enumerate(self.cells):
             frame = tk.Frame(self.scrollable_frame)
