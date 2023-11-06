c8a5cfbe03edfaa785d1aca0322444b0ba38aed8
CarmenJuaristi <cjuaristip22@fpcoruna.afundacion.org>
2023-11-06 17:23:07 +0100

    Completada tarea 2 y corregida tarea 1

---
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -0,0 +1,26 @@
+package com.example.mycatalog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CatalogActivity extends AppCompatActivity {
+    private Button boton;
+    private Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catalog_activity);
+        boton = findViewById(R.id.boton1);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+}
