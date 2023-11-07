5f001a62ef04a23ed19e6c71b10ebac3bf59f620
CarmenJuaristi <cjuaristip22@fpcoruna.afundacion.org>
2023-11-07 09:06:43 +0100

    Completada tarea 3, ya tenemos título, descripción, foto redonda y botón de me gusta

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CatalogActivity extends AppCompatActivity {
+    //Aquí inicializamos las variables boton que es de tipo boton y context
     private Button boton;
     private Context context = this;
 
@@ -14,7 +17,10 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalog_activity);
+        //Aquí lo que hacemos es que la variable que inicializamos antes tenga las propiedades que pusimos en el xml, para ello
+        // usamos el findViewById y ponemor la id que le dimos en el xml
         boton = findViewById(R.id.boton1);
+        // A partir de aquí lo que hacemos es que cuando el usuario pulse el botón se inicie la clase DetailActivity
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,6 +1,12 @@
 package com.example.mycatalog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class DetailActivity extends AppCompatActivity {
@@ -8,6 +14,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
+
     }
 
 }
